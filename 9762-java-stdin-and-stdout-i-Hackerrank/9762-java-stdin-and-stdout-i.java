27c87ba2c1223e91b6ddf0a8e4675a15177e64c0
akifullakhanpc3-max <akifullakhanpc3@gmail.com>
2026-09-08 22:02:57 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 9762-java-stdin-and-stdout-i-Hackerrank/9762-java-stdin-and-stdout-i.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        // String str = sc.nextLine();
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        
+        //System.out.print("myString is: "+str+"\n myInt is: "+number);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
