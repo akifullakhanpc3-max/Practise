9baaf525ce1ff2da5f09fedbb9613daa7d7c1787
akifullakhanpc3-max <akifullakhanpc3@gmail.com>
2026-09-10 14:51:55 +0530

    Force committed solution - Time: O(1), Space: O(1)

---
+++ 9458-java-stdin-and-stdout-ii-Hackerrank/9458-java-stdin-and-stdout-ii.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        double db = sc.nextDouble();
+        sc.nextLine();
+        String str = sc.nextLine();
+        System.out.println("String: " +str);
+        System.out.println("Double: "+ db);
+        System.out.println("Int: "+a);
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
